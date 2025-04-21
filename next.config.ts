97a43c38df19b4e0a123fd38f710b4aede28c106
Rigoberto Hernandez <142679865+RigobertoEHA1@users.noreply.github.com>
2025-04-21 00:18:20 -0600

    das
    asd

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  images: {
+    domains: ['i.ibb.co'],
+  },
+};
 
 export default nextConfig;
